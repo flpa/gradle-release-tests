eb0637407cf6381a18fa4191a37e8bfeeb953061
flpa <flpa.dev@zoho.com>
2016-02-08 21:02:12 +0100

    code change

--- src/main/java/io/github/flpa/gradle-release-test/Main.java
+++ src/main/java/io/github/flpa/gradle-release-test/Main.java
@@ -3,6 +3,6 @@ package io.github.flpa.gradlereleasetest;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Goings");
+		System.out.println("Goings v7");
 	}
 }
